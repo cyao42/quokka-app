9681883289089911e1f8bdbcccfb81c2fd1f9a96
Thomas Fleming <thf5@duke.edu>
2016-11-11 04:48:21 +0000

    tomcommit

--- database/create.sql
+++ database/create.sql
@@ -1,36 +1,36 @@
-CREATE TABLE SchoolUser
+CREATE TABLE Users
 (u_id INTEGER NOT NULL PRIMARY KEY,
  name VARCHAR(256) NOT NULL,
- phone VARCHAR(10) NOT NULL,
+ phone VARCHAR(15) NOT NULL,
  email VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE Professor
-(u_id INTEGER NOT NULL UNIQUE PRIMARY KEY REFERENCES SchoolUser(u_id),
+(u_id INTEGER NOT NULL UNIQUE PRIMARY KEY REFERENCES Users(u_id),
  name VARCHAR(256) NOT NULL,
- phone VARCHAR(10) NOT NULL,
+ phone VARCHAR(15) NOT NULL,
  email VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE Student
-(u_id INTEGER NOT NULL UNIQUE PRIMARY KEY REFERENCES SchoolUser(u_id),
+(u_id INTEGER NOT NULL UNIQUE PRIMARY KEY REFERENCES Users(u_id),
  name VARCHAR(256) NOT NULL,
- phone VARCHAR(10) NOT NULL,
+ phone VARCHAR(15) NOT NULL,
  email VARCHAR(256) NOT NULL,
  first_major VARCHAR(256), 
  second_major VARCHAR(256),
- grad_year INTEGER NOT CHECK (grad_year > 0)
+ grad_year INTEGER CHECK (grad_year > 0)
 );
 
-CREATE TABLE SchoolGroup
-(g_id INTEGER NOT NULL UNIQUE PRIMARY KEY,
- group_name VARCHAR(256) NOT NULL
+CREATE TABLE Groups
+(group_name VARCHAR(256) NOT NULL,
+g_id INTEGER NOT NULL UNIQUE PRIMARY KEY
 );
 
 CREATE TABLE MemberOf
-(u_id INTEGER NOT NULL UNIQUE REFERENCES SchoolUser(u_id),
- g_id INTEGER NOT NULL REFERENCES SchoolGroup(g_id),
- is_leader CHAR(1) NOT NULL CHECK (is_leader IN ('t', 'f')),
+(u_id INTEGER NOT NULL UNIQUE REFERENCES Users(u_id),
+ g_id INTEGER NOT NULL REFERENCES Groups(g_id),
+ is_leader CHAR(3) NOT NULL CHECK (is_leader IN ('yes', 'no')),
  PRIMARY KEY (u_id,g_id)
 );
 
@@ -60,7 +60,7 @@ FOREIGN KEY (class_code, class_semester, university_name, university_location) R
 );
 
 CREATE TABLE RegisteredWith
-(u_id INTEGER NOT NULL UNIQUE REFERENCES SchoolUser(u_id),
+(u_id INTEGER NOT NULL UNIQUE REFERENCES Users(u_id),
  section_number INTEGER NOT NULL,
  class_code VARCHAR(256) NOT NULL,
  class_semester VARCHAR(265) NOT NULL,
@@ -73,20 +73,11 @@ REFERENCES Section (section_number, class_code, class_semester, university_name,
 
 CREATE TABLE Add 
 (j_id INTEGER NOT NULL PRIMARY KEY,
+ g_id INTEGER REFERENCES Groups(g_id),
  message VARCHAR(1000),
- approved CHAR(1) NOT NULL CHECK (approved IN ('y', 'n'))
-);
-
-CREATE TABLE SentTo 
-(j_id INTEGER NOT NULL REFERENCES Add(j_id),
- u_id INTEGER NOT NULL REFERENCES SchoolUser(u_id),
- PRIMARY KEY (j_id, u_id)
-);
-
-CREATE TABLE SentBY 
-(j_id INTEGER NOT NULL REFERENCES Add(j_id),
- u_id INTEGER NOT NULL REFERENCES SchoolUser(u_id),
- PRIMARY KEY (j_id, u_id)
+ approved CHAR(3) NOT NULL CHECK (approved IN ('yes', 'no')),
+ sent_to INTEGER REFERENCES Users(u_id),
+ sent_by INTEGER REFERENCES Users(u_id)
 );
 
 CREATE TABLE ProjectAssignment
@@ -110,64 +101,65 @@ REFERENCES Section (section_number, class_code, class_semester, university_name,
 );
 
 CREATE TABLE Post
-(assignment_id INTEGER NOT NULL PRIMARY KEY REFERENCES ProjectAssignment(assignment_id),
- time_posted INTEGER NOT NULL,
- message VARCHAR(1000)
+(assignment_id INTEGER NOT NULL REFERENCES ProjectAssignment(assignment_id),
+ time_posted VARCHAR(100) NOT NULL,
+ message VARCHAR(1000),
+ PRIMARY KEY (assignment_id, time_posted)
 );
 
 CREATE TABLE NeedTeamPost
 (assignment_id INTEGER NOT NULL REFERENCES ProjectAssignment(assignment_id),
- u_id INTEGER NOT NULL REFERENCES SchoolUser(u_id),
+ u_id INTEGER NOT NULL REFERENCES Users(u_id),
  PRIMARY KEY (assignment_id, u_id)
 );
 
 CREATE TABLE NeedMemberPost
 (assignment_id INTEGER NOT NULL REFERENCES ProjectAssignment(assignment_id),
- g_id INTEGER NOT NULL REFERENCES SchoolGroup(g_id),
+ g_id INTEGER NOT NULL REFERENCES Groups(g_id),
  PRIMARY KEY (assignment_id, g_id)
 );
 
 CREATE TABLE ProjectGroup
-(g_id INTEGER NOT NULL PRIMARY KEY REFERENCES SchoolGroup(g_id),
+(g_id INTEGER NOT NULL PRIMARY KEY REFERENCES Groups(g_id),
  name VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE StudyGroup
-(g_id INTEGER NOT NULL PRIMARY KEY REFERENCES SchoolGroup(g_id),
+(g_id INTEGER NOT NULL PRIMARY KEY REFERENCES Groups(g_id),
  name VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE WorkingOn
-(assignment_id INTEGER NOT NULL REFERENCES ProjectAssignment(assignment_id),
- g_id INTEGER NOT NULL REFERENCES ProjectGroup(g_id),
- PRIMARY KEY (assignment_id, g_id)
+(g_id INTEGER NOT NULL REFERENCES ProjectGroup(g_id),
+ assignment_id INTEGER NOT NULL REFERENCES ProjectAssignment(assignment_id),
+ PRIMARY KEY (g_id, assignment_id)
 );
 
 CREATE TABLE StudyingFor 
-(g_id INTEGER NOT NULL REFERENCES SchoolGroup(g_id),
+(g_id INTEGER NOT NULL REFERENCES Groups(g_id),
+ section_number INTEGER,
  class_code VARCHAR(256) NOT NULL,
  class_semester VARCHAR(256) NOT NULL,
  university_name VARCHAR(256) NOT NULL,
  university_location VARCHAR(256) NOT NULL,
- PRIMARY KEY (g_id,class_code,class_semester,university_name,university_location),
- FOREIGN KEY (class_code, class_semester, university_name, university_location)
- REFERENCES Class(class_code, class_semester, university_name, university_location)
+ PRIMARY KEY (g_id,section_number,class_code,class_semester,university_name,university_location),
+ FOREIGN KEY (section_number,class_code, class_semester, university_name, university_location)
+ REFERENCES Section(section_number,class_code, class_semester, university_name, university_location)
 );
 
-CREATE TABLE Bar(name VARCHAR(20) NOT NULL PRIMARY KEY,
-                 address VARCHAR(20));
-CREATE TABLE Beer(name VARCHAR(20) NOT NULL PRIMARY KEY,
-                  brewer VARCHAR(20));
-CREATE TABLE Drinker(name VARCHAR(20) NOT NULL PRIMARY KEY,
-                     address VARCHAR(20));
-CREATE TABLE Frequents(drinker VARCHAR(20) NOT NULL REFERENCES Drinker(name),
-                       bar VARCHAR(20) NOT NULL REFERENCES Bar(name),
-                       times_a_week SMALLINT CHECK(times_a_week > 0),
-                       PRIMARY KEY(drinker, bar));
-CREATE TABLE Serves(bar VARCHAR(20) NOT NULL REFERENCES Bar(name),
-                    beer VARCHAR(20) NOT NULL REFERENCES Beer(name),
-                    price DECIMAL(5,2) CHECK(price > 0),
-                    PRIMARY KEY(bar, beer));
-CREATE TABLE Likes(drinker VARCHAR(20) NOT NULL REFERENCES Drinker(name),
-                   beer VARCHAR(20) NOT NULL REFERENCES Beer(name),
-                   PRIMARY KEY(drinker, beer));
+--List the names and groups of all leaders.--
+
+SELECT Users.name, group_name FROM Users, Groups, MemberOf
+WHERE MemberOf.u_id = Users.u_id AND MemberOf.g_id = Groups.g_id AND MemberOf.is_leader = 'yes';
+
+--List names of all users who have posted that they need a team, but are now part of a group.--
+
+SELECT Users.name FROM Users NATURAL JOIN NeedTeamPost NATURAL JOIN MemberOf NATURAL JOIN Groups;
+
+--List the names of all users who are not part of a group.--
+
+SELECT Users.name FROM Users EXCEPT (SELECT Users.name FROM Users NATURAL JOIN MemberOf NATURAL JOIN Groups);
+
+--List the project descriptions of projects that have been assigned for CS316.
+
+SELECT ProjectAssignment.description FROM ProjectAssignment NATURAL JOIN AssignedTo WHERE AssignedTo.class_code = '431';
--- models.py
+++ models.py
@@ -1,17 +1,17 @@
 from sqlalchemy import sql, orm
 from app import db
 
-class SchoolUser(db.Model):
-    __tablename__ = 'schooluser'
+class Users(db.Model):
+    __tablename__ = 'users'
     u_id = db.Column('u_id', db.Integer(), primary_key=True)
     name = db.Column('name', db.String(256))
-    phone = db.Column('phone', db.String(10))
+    phone = db.Column('phone', db.String(15))
     email = db.Column('email', db.String(256))
     @staticmethod
     def addNew(name, phone, email, user_type):
         try:
-            u_id = db.session.query(SchoolUser).count()+1
-            db.session.execute('INSERT INTO schooluser VALUES(:u_id, :name, :phone, :email)',
+            u_id = db.session.query(Users).count()+1
+            db.session.execute('INSERT INTO Users VALUES(:u_id, :name, :phone, :email)',
                                dict(u_id=u_id, name=name, phone=phone, email=email))
             if user_type == 'pro':
                 db.session.execute('INSERT INTO professor VALUES(:u_id, :name, :phone, :email)',
@@ -26,14 +26,14 @@ class SchoolUser(db.Model):
 
 class Professor(db.Model):
     __tablename__ = 'professor'
-    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('schooluser.u_id'), primary_key=True)
+    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('Users.u_id'), primary_key=True)
     name = db.Column('name', db.String(256))
     phone = db.Column('phone', db.Integer())
     email = db.Column('email', db.String(256))   
 
 class Student(db.Model):
     __tablename__ = 'student'
-    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('schooluser.u_id'), primary_key=True)
+    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('Users.u_id'), primary_key=True)
     name = db.Column('name', db.String(256))
     phone = db.Column('phone', db.Integer())
     email = db.Column('email', db.String(256))  
@@ -41,15 +41,15 @@ class Student(db.Model):
     second_major = db.Column('second_major', db.String(256))
     grad_year = db.Column('grad_year', db.Integer())  
 
-class SchoolGroup(db.Model):
-    __tablename__ = 'schoolgroup'
-    g_id = db.Column('g_id', db.Integer(), primary_key=True)
+class Groups(db.Model):
+    __tablename__ = 'groups'
     group_name = db.Column('group_name', db.String(256))
+    g_id = db.Column('g_id', db.Integer(), primary_key=True)
 
 class MemberOf(db.Model):
     __tablename__ = 'memberof'
-    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('schooluser.u_id'), primary_key=True)
-    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('schoolgroup.g_id'), primary_key=True)
+    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('Users.u_id'), primary_key=True)
+    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('Groups.g_id'), primary_key=True)
     is_leader = db.Column('is_leader', db.Boolean())
 
 class University(db.Model):
@@ -74,7 +74,7 @@ class Section(db.Model):
 
 class RegisteredWith(db.Model):
     __tablename__ = 'registeredwith'
-    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('schooluser.u_id'), primary_key=True)    
+    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('Users.u_id'), primary_key=True)    
     section_number = db.Column('section_number', db.Integer(), db.ForeignKey('section.section_number'), primary_key=True)
     course_code = db.Column('course_code', db.String(256), db.ForeignKey('course.course_code'), primary_key=True)
     course_semester = db.Column('course_semester', db.String(256), db.ForeignKey('course.course_semester'), primary_key=True)
@@ -84,18 +84,21 @@ class RegisteredWith(db.Model):
 class Add(db.Model):
     __tablename__ = 'join'
     j_id = db.Column('j_id', db.Integer(), primary_key=True)
+    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('groups.g_id'))
     message = db.Column('message', db.String(1000))
     approved = db.Column('approved', db.String(1))
+    sent_to = db.Column('sent_to', db.Integer(), db.ForeignKey('users.u_id'))
+    sent_by = db.Column('sent_by', db.Integer(), db.ForeignKey('users.u_id'))
 
 class SentTo(db.Model):
     __tablename__ = 'sentto'
     j_id = db.Column('j_id', db.Integer(), db.ForeignKey('join.j_id'), primary_key=True)    
-    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('schooluser.u_id'), primary_key=True)  
+    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('Users.u_id'), primary_key=True)  
 
 class SentBy(db.Model):
     __tablename__ = 'sentby'
     j_id = db.Column('j_id', db.Integer(), db.ForeignKey('join.j_id'), primary_key=True)    
-    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('schooluser.u_id'), primary_key=True)    
+    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('Users.u_id'), primary_key=True)    
     
 class ProjectAssignment(db.Model):
     __tablename__ = 'projectassignment'
@@ -117,125 +120,39 @@ class AssignedTo(db.Model):
 class Post(db.Model):
     __tablename__ = 'post'
     assignment_id = db.Column('assignment_id', db.Integer(), db.ForeignKey('projectassignment.assignment_id'), primary_key=True) 
-    time_posted = db.Column('time_posted', db.Integer())
+    time_posted = db.Column('time_posted', db.String(), primary_key=True)
     message = db.Column('message', db.String(1000))
 
 class NeedTeamPost(db.Model):
     __tablename__ = 'needteampost'
     assignment_id = db.Column('assignment_id', db.Integer(), db.ForeignKey('projectassignment.assignment_id'), primary_key=True)   
-    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('schooluser.u_id'), primary_key=True)  
+    u_id = db.Column('u_id', db.Integer(), db.ForeignKey('Users.u_id'), primary_key=True)  
 
 class NeedMemberPost(db.Model):
     __tablename__ = 'needmemberpost'
     assignment_id = db.Column('assignment_id', db.Integer(), db.ForeignKey('projectassignment.assignment_id'), primary_key=True)   
-    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('schoolgroup.g_id'), primary_key=True)
+    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('Groups.g_id'), primary_key=True)
 
 class ProjectGroup(db.Model):
     __tablename__ = 'projectgroup'
-    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('schoolgroup.g_id'), primary_key=True)  
+    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('Groups.g_id'), primary_key=True)  
     name = db.Column('name', db.String(256))
 
 class StudyGroup(db.Model):
     __tablename__ = 'studygroup'
-    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('schoolgroup.g_id'), primary_key=True)  
+    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('Groups.g_id'), primary_key=True)  
     name = db.Column('name', db.String(256))
 
 class WorkingOn(db.Model):
     __tablename__ = 'workingon'
-    assignment_id = db.Column('assignment_id', db.Integer(), db.ForeignKey('projectassignment.assignment_id'), primary_key=True)
-    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('schoolgroup.g_id'), primary_key=True)         
+    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('Groups.g_id'), primary_key=True)
+    assignment_id = db.Column('assignment_id', db.Integer(), db.ForeignKey('projectassignment.assignment_id'), primary_key=True)         
 
 class StudyingFor(db.Model):
     __tablename__ = 'studyingfor'
-    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('schoolgroup.g_id'), primary_key=True)
+    g_id = db.Column('g_id', db.Integer(), db.ForeignKey('Groups.g_id'), primary_key=True)
+    section_number = db.Column('section_number', db.Integer(), db.ForeignKey('Section.section_number'), primary_key=True)
     course_code = db.Column('course_code', db.String(256), db.ForeignKey('course.course_code'), primary_key=True)
     course_semester = db.Column('course_semester', db.String(256), db.ForeignKey('course.course_semester'), primary_key=True) 
     university_name = db.Column('university_name', db.String(256), db.ForeignKey('course.university_name'), primary_key=True)
     university_location = db.Column('university_location', db.String(256), db.ForeignKey('course.university_location'), primary_key=True)
-
-class Drinker(db.Model):
-    __tablename__ = 'drinker'
-    name = db.Column('name', db.String(20), primary_key=True)
-    address = db.Column('address', db.String(20))
-    likes = orm.relationship('Likes')
-    frequents = orm.relationship('Frequents')
-    @staticmethod
-    def addNew(name, address, beers_liked, bars_frequented):
-        try:
-            db.session.execute('INSERT INTO drinker VALUES(:name, :address)',
-                               dict(name=name, address=address))
-            for beer in beers_liked:
-                db.session.execute('INSERT INTO likes VALUES(:drinker, :beer)',
-                                   dict(drinker=name, beer=beer))
-            for bar, times_a_week in bars_frequented:
-                db.session.execute('INSERT INTO frequents'
-                                   ' VALUES(:drinker, :bar, :times_a_week)',
-                                   dict(drinker=name, bar=bar,
-                                        times_a_week=times_a_week))
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            raise e
-
-    @staticmethod
-    def edit(old_name, name, address, beers_liked, bars_frequented):
-        try:
-            db.session.execute('DELETE FROM likes WHERE drinker = :name',
-                               dict(name=old_name))
-            db.session.execute('DELETE FROM frequents WHERE drinker = :name',
-                               dict(name=old_name))
-            db.session.execute('UPDATE drinker SET name = :name, address = :address'
-                               ' WHERE name = :old_name',
-                               dict(old_name=old_name, name=name, address=address))
-            for beer in beers_liked:
-                db.session.execute('INSERT INTO likes VALUES(:drinker, :beer)',
-                                   dict(drinker=name, beer=beer))
-            for bar, times_a_week in bars_frequented:
-                db.session.execute('INSERT INTO frequents'
-                                   ' VALUES(:drinker, :bar, :times_a_week)',
-                                   dict(drinker=name, bar=bar,
-                                        times_a_week=times_a_week))
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            raise e
-
-class Beer(db.Model):
-    __tablename__ = 'beer'
-    name = db.Column('name', db.String(20), primary_key=True)
-    brewer = db.Column('brewer', db.String(20))
-
-class Bar(db.Model):
-    __tablename__ = 'bar'
-    name = db.Column('name', db.String(20), primary_key=True)
-    address = db.Column('address', db.String(20))
-    serves = orm.relationship('Serves')
-
-class Likes(db.Model):
-    __tablename__ = 'likes'
-    drinker = db.Column('drinker', db.String(20),
-                        db.ForeignKey('drinker.name'),
-                        primary_key=True)
-    beer = db.Column('beer', db.String(20),
-                     db.ForeignKey('beer.name'),
-                     primary_key=True)
-
-class Serves(db.Model):
-    __tablename__ = 'serves'
-    bar = db.Column('bar', db.String(20),
-                    db.ForeignKey('bar.name'),
-                    primary_key=True)
-    beer = db.Column('beer', db.String(20),
-                     db.ForeignKey('beer.name'),
-                     primary_key=True)
-    price = db.Column('price', db.Float())
-
-class Frequents(db.Model):
-    __tablename__ = 'frequents'
-    drinker = db.Column('drinker', db.String(20),
-                        db.ForeignKey('drinker.name'),
-                        primary_key=True)
-    bar = db.Column('bar', db.String(20),
-                    db.ForeignKey('bar.name'),
-                    primary_key=True)
-    times_a_week = db.Column('times_a_week', db.Integer())
