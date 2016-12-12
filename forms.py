f12df242071d100aa84fdd47329b8ed5a05d710a
Roxanne Baker <reb36@duke.edu>
2016-12-12 10:40:39 +0000

    Change new group form to use assignment description instead of id

--- forms.py
+++ forms.py
@@ -9,7 +9,7 @@ class GroupNewFormFactory:
         class F(FlaskForm):
             name = StringField(default='')
             assign_options = [("none", "Study Group")]
-            assign_options.extend([(str(assign.assignment_id), assign.assignment_id) for assign in assignments])
+            assign_options.extend([(str(assign.assignment_id), assign.description) for assign in assignments])
             assign = SelectField('Assignment', choices=assign_options)
         return F()
 
